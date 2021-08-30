4613692e11887546fb3f1ddc1b549d73f71356e6
thuanho195 <boyover055@gmail.com>
2021-08-30 22:48:42 +0700

    no message

--- platform/themes/main/views/sections/section5.blade.php
+++ platform/themes/main/views/sections/section5.blade.php
@@ -19,7 +19,7 @@
                 </div>
                 <div id="section6b" class="tab-pane fade tab-content-s6-item">
                     <div class="img-pri" data-aos="fade-right" data-aos-easing="ease-out-cubic" data-aos-duration="1000">
-                        <img src="{{ Theme::asset()->url('images/section5/ngoaithat2.jpg') }}">
+                        <img src="{{ Theme::asset()->url('images/section5/ngoaithat2a.jpg') }}">
                     </div>
                     <div class="content-ab" data-aos="fade-left" data-aos-easing="ease-out-cubic" data-aos-duration="1000">
                         <p class="font-buenos-light font20">Cerato được làm mới thể thao hơn, bóng bẩy hơn và năng động hơn từ trên xuống dưới. Phong cách táo bạo có ở khắp mọi nơi, 
