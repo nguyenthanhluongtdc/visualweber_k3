7bdee4ede9b452736cf9e9af4fe76da600726ce4
thuanho195 <boyover055@gmail.com>
2021-09-15 21:29:02 +0700

    sass

--- platform/themes/main/views/sections/section6.blade.php
+++ platform/themes/main/views/sections/section6.blade.php
@@ -3,8 +3,19 @@
 </div> --}}
 @if (has_field($page, 'hinh_background_s6'))
 
-<div class="section section6 fp-auto-height-responsive fp-section fp-completely" data-anchor="hinh-anh">
+<div class="section section6 fp-auto-height-responsive fp-section fp-completely" data-anchor="hinh-anh" style="background-image: url('{{  RvMedia::getImageUrl(get_field($page, 'hinh_background_s6')) }}')">
     {{-- <img src="{{  RvMedia::getImageUrl(get_field($page, 'hinh_background_s6')) }}"> --}}
-    <img src="{{  RvMedia::getImageUrl(get_field($page, 'hinh_background_s6')) }}" class="img-mobie">
+
+    <div class="content">
+        <div class="top">
+            <img src="{{ Theme::asset()->url('images/logok3so1.png') }}" alt="logo">
+        </div>
+        <div class="bottom">
+            <h3 class="font60 font-kia-light">KẾT NỐI CÔNG NGHỆ THÔNG MINH</h3>
+            <div class="link"><a href="#" class="font20 font-kia-light">KHÁM PHÁ NGAY</a></div>
+        </div>
+    </div>
+
+    <img src="{{ Theme::asset()->url('images/kv3.jpg') }}" class="img-mobie">
 </div>
 @endif
