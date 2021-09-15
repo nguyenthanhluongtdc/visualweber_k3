273b635a0ff95cf90958ce9a881f79ac24cca059
thuanho195 <boyover055@gmail.com>
2021-09-15 21:02:03 +0700

    sass

--- platform/themes/main/views/sections/section6.blade.php
+++ platform/themes/main/views/sections/section6.blade.php
@@ -3,7 +3,7 @@
 </div> --}}
 @if (has_field($page, 'hinh_background_s6'))
 
-<div class="section section6 fp-auto-height-responsive fp-section fp-completely" data-anchor="hinh-anh" style="background-image: url('{{  RvMedia::getImageUrl(get_field($page, 'hinh_background_s6')) }}')">
+<div class="section section6 fp-auto-height-responsive fp-section fp-completely" data-anchor="hinh-anh">
     {{-- <img src="{{  RvMedia::getImageUrl(get_field($page, 'hinh_background_s6')) }}"> --}}
     <img src="{{  RvMedia::getImageUrl(get_field($page, 'hinh_background_s6')) }}" class="img-mobie">
 </div>
