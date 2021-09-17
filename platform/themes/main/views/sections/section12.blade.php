93a8c309588671a6854ebb720adb4ba9ec6876d7
thuanho195 <boyover055@gmail.com>
2021-09-17 21:16:39 +0700

    sass

--- platform/themes/main/views/sections/section12.blade.php
+++ platform/themes/main/views/sections/section12.blade.php
@@ -1,4 +1,4 @@
-<div class="section p-auto-height-responsive fp-auto-height section12 fp-auto-height-responsive fp-section fp-table fp-completely" data-anchor="footer">
+<div class="section p-auto-height-responsive fp-auto-height section12 fp-auto-height-responsive fp-section fp-table fp-completely" data-anchor="lien-he">
     
     <div class="section section-12-top p-auto-height-responsive fp-auto-height fp-auto-height-responsive fp-section fp-table fp-completely" data-anchor="lien-he"
     style="background-image: url('{{Theme::asset()->url('images/bglienhe.jpg') }}')"
