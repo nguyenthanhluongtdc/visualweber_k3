e27cea21c8f0f1115854f67a6d378127d414fb59
thuanho195 <boyover055@gmail.com>
2021-09-17 21:07:37 +0700

    sass

--- platform/themes/main/views/sections/section10.blade.php
+++ platform/themes/main/views/sections/section10.blade.php
@@ -1,7 +1,7 @@
 <div class="section section10 fp-auto-height-responsive fp-section fp-completely" data-anchor="san-pham">
     <div class="container-remake section10-wrap" >
-        {{-- <h2 class="text-uppercase font-kia-bold font80 text-center mb-0">ĐẶT XE TRỰC TUYẾN</h2> --}}
-        <h3 class="font60 font-kia-bold" >Các dòng sản phẩm</h3>
+        <h2 class="text-uppercase font-kia-bold font60 text-center mb-0">ĐẶT XE TRỰC TUYẾN</h2>
+        <h3 class="font40 font-kia-bold" >Các dòng sản phẩm</h3>
         <div class="list-section10">
             <div class="item">
                 <div class="top">
@@ -12,24 +12,28 @@
                     <img src="{{ Theme::asset()->url('images/section10/xepri.png') }}">
                     <div class="content-item">
                         <p class="title font20 font-kia-bold">Tính năng nổi bật</p>
-                        <ul class="list-content scrollable-content">
-                            <li class="font20 font-kia-re">- Màn hình đa thông tin 4.2”</li>
-                            <li class="font20 font-kia-re">- Màn hình giải trí AVN 10.25”</li>
-                            <li class="font20 font-kia-re">- Sưởi và làm mát hàng ghế trước</li>
-                            <li class="font20 font-kia-re">- Gương chiếu hậu chống chói tự động ECM</li>
-                            <li class="font20 font-kia-re">- Nhớ 2 vị trí ghế lái</li>
-                            <li class="font20 font-kia-re">- 6 túi khí an toàn, cảm biến hỗ trợ đỗ xe trước và sau</li>
+                        <ul class="list-content scrollable-content font18">
+                            <li class="font-kia-re">- Màn hình đa thông tin 4.2”</li>
+                            <li class="font-kia-re">- Màn hình giải trí AVN 10.25”</li>
+                            <li class="font-kia-re">- Sưởi và làm mát hàng ghế trước</li>
+                            <li class="font-kia-re">- Gương chiếu hậu chống chói tự động ECM</li>
+                            <li class="font-kia-re">- Nhớ 2 vị trí ghế lái</li>
+                            <li class="font-kia-re">- 6 túi khí an toàn, cảm biến hỗ trợ đỗ xe trước và sau</li>
                         </ul>
-                        <div class="row-price">
+                        <div class="row-price font20 font-kia-bold">
                             <strong class="text-uppercase font-kia-bold">
                                 GIÁ NIÊM YẾT: 
                             </strong>
                             <b class="price">xxx.xxx.xxx VNĐ</b>
                         </div>
                         <div class="content-bottom">
-                            <div class="register font-kia-bold font20 font-kia-re">
+                            <a class="register font-kia-bold font20 font-kia-re" data-src="#animatedModal101" href="javascript:;" data-fancybox="muaxe1">
                                 BẮT ĐẦU NGAY
-                               </div>
+                            </a>
+                            <div style="display: none;" id="animatedModal101" class="animated-modal">
+
+                                <img src="{{ Theme::asset()->url('images/section10/popupmx.jpg') }}">
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -43,23 +47,23 @@
                     <img src="{{ Theme::asset()->url('images/section10/2luxu.png') }}">
                     <div class="content-item">
                         <p class="title font20 font-kia-bold">Tính năng nổi bật </p>
-                        <ul class="list-content scrollable-content">
-                            <li class="font20 font-kia-re">- Màn hình đa thông tin 3.5”</li>
-                            <li class="font20 font-kia-re">- Màn hình giải trí AVN 8”</li>
-                            <li class="font20 font-kia-re">- Tính năng khởi động từ xa</li>
-                            <li class="font20 font-kia-re">- Ghế lái chỉnh điện, cửa sổ trời</li>
-                            <li class="font20 font-kia-re">- Cảm biến hỗ trợ đỗ xe sau</li>
+                        <ul class="list-content scrollable-content font18">
+                            <li class="font-kia-re">- Màn hình đa thông tin 3.5”</li>
+                            <li class="font-kia-re">- Màn hình giải trí AVN 8”</li>
+                            <li class="font-kia-re">- Tính năng khởi động từ xa</li>
+                            <li class="font-kia-re">- Ghế lái chỉnh điện, cửa sổ trời</li>
+                            <li class="font-kia-re">- Cảm biến hỗ trợ đỗ xe sau</li>
                         </ul>
-                        <div class="row-price">
+                        <div class="row-price font20 font-kia-bold">
                             <strong class="text-uppercase font-kia-bold">
                                 GIÁ NIÊM YẾT: 
                             </strong>
                             <b class="price">xxx.xxx.xxx VNĐ</b>
                         </div>
                         <div class="content-bottom">
-                            <div class="register font-kia-bold font20 font-kia-re">
+                            <a href="#" class="register font-kia-bold font20 font-kia-re">
                                 BẮT ĐẦU NGAY
-                               </div>
+                            </a>
                         </div>
                     </div>
                 </div>
@@ -73,23 +77,23 @@
                     <img src="{{ Theme::asset()->url('images/section10/3mt.png') }}">
                     <div class="content-item">
                         <p class="title font20 font-kia-bold">Tính năng nổi bật </p>
-                        <ul class="list-content scrollable-content">
-                            <li class="font20 font-kia-re">- Màn hình giải trí AVN 8”</li>
-                            <li class="font20 font-kia-re">- Hệ thống âm thanh 6 loa</li>
-                            <li class="font20 font-kia-re">- Cửa gió cho hàng ghế 2</li>
-                            <li class="font20 font-kia-re">- Hệ thống cân bằng điện tử ESC + HAC</li>
-                            <li class="font20 font-kia-re">- Camera lùi</li>
+                        <ul class="list-content scrollable-content font18">
+                            <li class="font-kia-re">- Màn hình giải trí AVN 8”</li>
+                            <li class="font-kia-re">- Hệ thống âm thanh 6 loa</li>
+                            <li class="font-kia-re">- Cửa gió cho hàng ghế 2</li>
+                            <li class="font-kia-re">- Hệ thống cân bằng điện tử ESC + HAC</li>
+                            <li class="font-kia-re">- Camera lùi</li>
                         </ul>
-                        <div class="row-price">
-                            <strong class="text-uppercase font-kia-bold">
+                        <div class="row-price font20 font-kia-bold">
+                            <strong class="text-uppercase  ">
                                 GIÁ NIÊM YẾT: 
                             </strong>
                             <b class="price">xxx.xxx.xxx VNĐ</b>
                         </div>
                         <div class="content-bottom">
-                           <div class="register font-kia-bold font20 font-kia-re">
+                           <a href="" class="register font-kia-bold font20 font-kia-re">
                             BẮT ĐẦU NGAY
-                           </div>
+                           </a>
                         </div>
                     </div>
                 </div>
