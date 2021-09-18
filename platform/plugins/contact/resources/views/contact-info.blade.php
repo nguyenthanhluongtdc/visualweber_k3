c7aeb54be2cde60f33e1ca5b449a8b7f53c46029
ASUS-UX430\kHA <kha.ph@visualweber.com>
2021-09-18 19:59:55 +0700

    update

--- platform/plugins/contact/resources/views/contact-info.blade.php
+++ platform/plugins/contact/resources/views/contact-info.blade.php
@@ -1,7 +1,8 @@
 @if ($contact)
-    <p>Thời gian: <i>{{ $contact->created_at }}</i></p>
-    <p>Họ và tên khách hàng: <i>{{ $contact->name }}</i></p>
+    <p>Thời gian: <i>{{ $contact->created_at ?? "N/A" }}</i></p>
+    <p>Họ và tên khách hàng: <i>{{ $contact->name ?? "N/A"}}</i></p>
     <p>Số điện thoại: <i>@if ($contact->phone) <a href="tel:{{ $contact->phone }}">{{ $contact->phone }}</a> @else N/A @endif</i></p>
     <p>Địa chỉ: <i>Tỉnh/Thành phố {{ $contact->city.', '.$contact->district.', '.$contact->ward.' ('.$contact->address.')' }}</i></p>
+    <p>Showroom: <i>{{ $contact->showroom ?? "N/A"}}</i></p>
 
 @endif
