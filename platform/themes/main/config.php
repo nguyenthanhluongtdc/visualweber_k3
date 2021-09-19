fbea4e8a2abb457f9f5ae6ede92b6c13bd2115c2
thuanho195 <boyover055@gmail.com>
2021-09-19 11:01:02 +0700

    sass

--- platform/themes/main/config.php
+++ platform/themes/main/config.php
@@ -48,23 +48,23 @@ return [
             $theme->asset()->add('bootstrap', '//cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css');
             $theme->asset()->add('fancybox', '//cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css');
             $theme->asset()->add('aos_style', '//unpkg.com/aos@2.3.1/dist/aos.css');
-            // $theme->asset()->add('animate_style', '//cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css');
-            // $theme->asset()->add('fullpage_css', '//cdnjs.cloudflare.com/ajax/libs/fullPage.js/3.0.9/fullpage.min.css');
+            $theme->asset()->add('animate_style', '//cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css');
+            $theme->asset()->add('fullpage_css', '//cdnjs.cloudflare.com/ajax/libs/fullPage.js/3.0.9/fullpage.min.css');
             $theme->asset()->add('dropdown_css', 'https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/components/dropdown.min.css');
-            // $theme->asset()->add('semantic', '//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css');
+            $theme->asset()->add('semantic', '//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css');
             $theme->asset()->add('select2', '//cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css');
             $theme->asset()->add('font-awesome', '//cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css');
-            // $theme->asset()->add('carousel', '//cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css');
-            // $theme->asset()->add('carousel_thumb', '//cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css');
+            $theme->asset()->add('carousel', '//cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css');
+            $theme->asset()->add('carousel_thumb', '//cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css');
             $theme->asset()->add('jquery-ui', '//ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/themes/base/jquery-ui.css');
             
             $theme->asset()->usePath()->add('style', 'css/common.css',[],[],time());
 
             $theme->asset()->container('header')->add('jquery', '//code.jquery.com/jquery-3.5.1.min.js');
             $theme->asset()->container('footer')->add('fancybox', '//cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js');
-            // $theme->asset()->container('footer')->add('semantic', '//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.js');
+            $theme->asset()->container('footer')->add('semantic', '//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.js');
             $theme->asset()->container('footer')->add('fullpage_js', '//cdnjs.cloudflare.com/ajax/libs/fullPage.js/3.0.9/fullpage.min.js');
-            // $theme->asset()->container('footer')->add('popper', '//cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js');
+            $theme->asset()->container('footer')->add('popper', '//cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js');
             $theme->asset()->container('footer')->add('bootstrap', '//maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js');
             $theme->asset()->container('footer')->add('aos_js', '//unpkg.com/aos@2.3.1/dist/aos.js');
             $theme->asset()->container('footer')->add('select2', '//cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js');
@@ -72,10 +72,10 @@ return [
             $theme->asset()->container('footer')->add('validate', '//cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.2/jquery.validate.min.js');
 			$theme->asset()->container('footer')->add('validate-method', '//cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.2/additional-methods.min.js');
             $theme->asset()->container('footer')->add('carousel', '//cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js'); 
-            // $theme->asset()->container('footer')->add('carousel_thumb', '//cdn.jsdelivr.net/npm/owl.carousel2.thumbs@0.1.8/dist/owl.carousel2.thumbs.min.js');          
+            $theme->asset()->container('footer')->add('carousel_thumb', '//cdn.jsdelivr.net/npm/owl.carousel2.thumbs@0.1.8/dist/owl.carousel2.thumbs.min.js');          
             $theme->asset()->container('footer')->add('jquery_ui', '//code.jquery.com/ui/1.10.3/jquery-ui.js');
-            // $theme->asset()->container('footer')->add('cloudimage', '//cdn.scaleflex.it/plugins/js-cloudimage-360-view/2.6.0/js-cloudimage-360-view.min.js');
-            // $theme->asset()->container('footer')->usePath()->add('script', 'js/common.js',[],[],time());
+            $theme->asset()->container('footer')->add('cloudimage', '//cdn.scaleflex.it/plugins/js-cloudimage-360-view/2.6.0/js-cloudimage-360-view.min.js');
+            $theme->asset()->container('footer')->usePath()->add('script', 'js/common.js',[],[],time());
             $theme->asset()->container('footer')->usePath()->add('script', 'js/fullpage.js',[],[],time());
         },
 
--- platform/themes/main/views/page.blade.php
+++ platform/themes/main/views/page.blade.php
@@ -1,88 +1,7 @@
 @php Theme::layout('default') @endphp
-{{-- {!! get_field($page, 'number_user') !!} --}}
-
-{{-- <div class="count-down">
-    
-    <video autoplay muted loop playsinline id="myVideo">
-        <source src="{{ Theme::asset()->url('images/section1/video.mp4') }}" type="video/mp4">
-    </video>
-    <div class="fixed-bg">
-
-    </div>
-    <div  class="animated-modal-tk">
-        <div class="popup-content">
-            <h2 class="font-sfu-black">CẢM ƠN BẠN ĐÃ ĐĂNG KÝ!</h2>
-            <div class="close-popup">
-                <i class="fas fa-times"></i>
-            </div>
-        </div>
-        
-    </div>
-    <div class="content-count-down">
-        <h3 class="font-sfu-black">SẮP XUẤT HIỆN</h3>
-        <ul class="time-countdown">
-            <li>
-                <p id="days" class="font-sfu-black"></p>
-                <p class="font-sfu-bold">ngày</p>
-            </li>
-            <li>
-                <p id="hours" class="font-sfu-black"></p>
-                <p class="font-sfu-bold">giờ</p>
-            </li>
-            <li>
-                <p id="minutes" class="font-sfu-black"></p>
-                <p class="font-sfu-bold">phút</p>
-            </li>
-            <li>
-                <p id="seconds" class="font-sfu-black"></p>
-                <p class="font-sfu-bold">giây</p>
-            </li>
-        </ul>
-
-        <h5 class="font-sfu-black">ĐĂNG KÝ NHẬN THÔNG TIN MỚI NHẤT</h5>
 
 
-        {!! Form::open(['route' => 'public.send.contact', 'method' => 'POST', 'class' => 'contact-form']) !!}
-            <div class="contact-form-row contact-form-row-top">
-                <div class="contact-column-6">
-                    <div class="contact-form-group">
-                        <input type="text" class="contact-form-input font20 font-buenos-light" name="name" value="{{ old('name') }}" id="contact_name"
-                            placeholder="Họ và tên">
-                            <p class="error-msg font-buenos-re">{{$errors->first('name')}}</p>
-                    </div>
-                </div>
-                <div class="contact-column-6">
-                    <div class="contact-form-group">
-                        <input type="text" class="contact-form-input font20 font-buenos-light" name="phone" value="{{ old('phone') }}" id="contact_phone"
-                               placeholder="Số điện thoại">
-                               <p class="error-msg font-buenos-re">{{$errors->first('phone')}}</p>
-                    </div>
-                </div>
-            </div>
-            <div class="contact-form-row">
-                <div class="contact-column-12">
-                    <div class="contact-form-group">
-                        <input type="text" class="contact-form-input font20 font-buenos-light" name="address" value="{{ old('address') }}" id="contact_address"
-                               placeholder="Địa chỉ">
-                               <p class="error-msg font-buenos-re">{{$errors->first('address')}}</p>
-                    </div>
-                </div>
-            </div>
-            <div class="contact-form-group-bt">
-                <button type="submit" class="contact-button font-buenos-bold font20">Gửi</button>
-            </div>
-            
-            <div class="contact-form-group">
-                <div class="contact-message contact-success-message" style="display: none"></div>
-                <div class="contact-message contact-error-message" style="display: none"></div>
-            </div>
-    {!! Form::close() !!}
-
-    </div>
-</div> --}}
-
-{{-- tạm ẩn fp  --}}
-{{-- <div id="fullpage" class="fullpage" data-section="{{$section ?? ''}}" class="d-none">
+<div id="fullpage" class="fullpage" data-section="{{$section ?? ''}}" class="d-none">
     @includeIf('theme.main::views.sections.section1')
     @includeIf('theme.main::views.sections.section2')
     @includeIf('theme.main::views.sections.section3')
@@ -95,7 +14,7 @@
     @includeIf('theme.main::views.sections.section10')
     @includeIf('theme.main::views.sections.section11')
     @includeIf('theme.main::views.sections.section12')
-</div> --}}
+</div>
 
 
 <script>
