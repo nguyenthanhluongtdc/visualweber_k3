c5070fc2bac45dc7f9cf889cc089bf6bff34de41
thuanho195 <boyover055@gmail.com>
2021-09-20 13:52:51 +0700

    sass

--- platform/themes/main/views/sections/section3.blade.php
+++ platform/themes/main/views/sections/section3.blade.php
@@ -94,7 +94,7 @@
                             {{-- Black --}}
                             <div id="tabcar02" class="container tab-pane item-car-color"><br>
                                 <div class="cloudimage-360" data-folder="/themes/main/images/360/black/"
-                                    data-filename="ABP_{index}.webp" data-amount="36" data-spin-reverse="true"></div>
+                                    data-filename="ABP_{index}.png" data-amount="36" data-spin-reverse="true"></div>
                                 <div class="ring360">
                                     <img src="{{ Theme::asset()->url('images/section3/360.png') }}" alt="360">
                                 </div>
