12e285f9b9fca4c1a0ae68213a5b25f8a7bee284
thuanho195 <boyover055@gmail.com>
2021-09-21 11:05:52 +0700

    no message

--- platform/themes/main/views/index.blade.php
+++ platform/themes/main/views/index.blade.php
@@ -180,7 +180,7 @@
 
 <script>
 // Set the date we're counting down to
-var countDownDate = new Date("Sep 22, 2021 15:00:00").getTime();
+var countDownDate = new Date("Sep 22, 2021 17:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
